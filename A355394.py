e8131be626ce94e2a21c0e3e37ea9e3f8082ad32
Lucas A. Brown <115374136+lucasaugustus@users.noreply.github.com>
2022-10-11 09:02:32 -0700

    Create A355394.py

---
+++ A355394.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+# Number of integer partitions of n such that, for all parts x, x - 1 or x + 1 is also a part.
+
+from labmath import *
+
+print("0 1")
+
+for n in count(1):
+    total = 0
+    for party in partgen(n):
+        flag = True
+        for x in party:
+            if ((x-1) not in party) and ((x+1) not in party): flag = False
+        if flag: total += 1
+    print(n, total)
