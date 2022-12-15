e367aa25d08035abb77eee394c9f586e3fb6c8e0
Lucas A. Brown <115374136+lucasaugustus@users.noreply.github.com>
2022-12-14 19:33:06 -0800

    Create A358903.py

---
+++ A358903.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+# A358903: Number of integer partitions of n whose parts have all different numbers of distinct prime factors
+
+from labmath import *       # Available via pip (https://pypi.org/project/labmath/)
+
+print("0 1")
+print("1 1")
+
+Omegas = [len(set(primefac(n))) for n in range(1000)]
+
+for n in count(2):
+    total = 0
+    for party in partgen(n):
+        for (k,p) in enumerate(party): party[k] = Omegas[p]
+        if len(party) == len(set(party)): total += 1
+    print(n, total)
