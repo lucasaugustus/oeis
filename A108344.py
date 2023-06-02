5a80a238e3ff9bf25f8380675b81a4c714dcd7dd
Lucas A. Brown <115374136+lucasaugustus@users.noreply.github.com>
2023-06-02 03:15:43 -0700

    Update A108344.py

--- A108344.py
+++ A108344.py
@@ -12,11 +12,7 @@ for n in count(1):
     for p in primes: Zpn[p] = pow(pow(10, 100, p) + 267, n, p)
     for k in count(2, 2):
         print('\b'*42, n, k, end='', flush=True)
-        #Zp = pow(10, 100, p) + 267
-        #Zpn = pow(Zp, n, p)
-        #if any((k * pow(pow(10, 100, p) + 267, n, p) + 1) % p == 0 for p in primegen(100)): continue
         if any((k * Zpn[p] + 1) % p == 0 for p in primes): continue
         if isprime(k * Zn + 1, tb=[]): break
-        #if isprime(k * Zn + 1): break
     assert isprime_nm1(k * Zn + 1, {Z:n})
     print()
