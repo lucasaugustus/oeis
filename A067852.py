496b2331ebbb4fcd1783a897ef99683571da1b38
Lucas A. Brown <115374136+lucasaugustus@users.noreply.github.com>
2024-09-22 13:35:17 -0700

    Create A067852.py

---
+++ A067852.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+from labmath import primegen, count
+
+pg = primegen()
+n = 0
+for k in count(1):
+    if k % 1000 == 0: print('\b'*42, k, end='', flush=True)
+    for _ in range(2*k - 1): p = next(pg)
+    if (p - 1) % k == 0:
+        n += 1
+        print('\b'*42, n, ' ', k, sep='')
